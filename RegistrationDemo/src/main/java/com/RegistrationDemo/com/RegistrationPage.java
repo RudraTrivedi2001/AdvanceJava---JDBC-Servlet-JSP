ec26ac3f036c691b641641c8edbd15d0727f7c63
Aniruddha Trivedi <aniruddhatrivediii@gmail.com>
2024-12-19 21:55:22 +0530

    Servlet Practice

---
+++ RegistrationDemo/src/main/java/com/RegistrationDemo/com/RegistrationPage.java
@@ -0,0 +1,41 @@
+package com.RegistrationDemo.com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class RegistrationPage
+ */
+public class RegistrationPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  String name =	request.getParameter("name");
+  String email = request.getParameter("email");
+  String phone = request.getParameter("number");
+  
+        if(name == null ||name.isEmpty() || email == null ||email.isEmpty() || phone == null || phone.isEmpty()) {
+        	response.sendRedirect("error.html");
+        }else {
+        	response.sendRedirect("success.html");
+        }
+  
+  
+  
+	}
+
+}
--- ServletLoginDemo/src/main/java/com/ServletDemo/java/LoginPage.java
+++ ServletLoginDemo/src/main/java/com/ServletDemo/java/LoginPage.java
@@ -1,4 +1,4 @@
-package com.annotation.java;
+package com.ServletDemo.java;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
